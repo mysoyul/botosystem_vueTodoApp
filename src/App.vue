39bc0d716ba5abe2d98a8c05dc3543c5726e2fa7
mysoyul <vega2k_@naver.com>
2021-11-21 10:51:03 +0900

    vue todo app

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput @addEvent="addTodo"></TodoInput>
+    <TodoList :todo-array="todoItems"></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+
+export default {
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  },
+  //상태변수
+  data() {
+    return {
+      //todoItems 배열 상태변수
+      todoItems: [],
+    };
+  },
+  //사용자정의 메서드
+  methods: {
+    addTodo(todoText) {
+      var obj = { completed: false, item: todoText };
+      console.log(obj);
+      //JSON.stringify는 object를 json string 으로 변환
+      localStorage.setItem(obj.item, JSON.stringify(obj));
+      //todoItems 상태변수에 객체저장
+      this.todoItems.push(obj);
+    },
+  },
+  /* life cycle method */
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          //console.log(typeof localStorage.getItem(localStorage.key(i)));
+          //console.log( JSON.parse(localStorage.getItem(localStorage.key(i))) );
+          //JSON.parse()는 json string을 object로 변환
+          const todoObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          this.todoItems.push(todoObj);
+        }
+      }
+    }
+  },
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" ref="newTodoItem" v-model="newTodoItem" @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+        <!-- <button @click="addTodo">add</button> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data:function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    //LifeCycle Method 재정의
+    mounted() {
+      //input 필드에 직접 접근해서 포커스 주기
+      this.$refs.newTodoItem.focus();
+    },
+    methods:{
+        addTodo() {
+            if (this.newTodoItem !== '') {
+                //addEvent 라는 이름을 갖는 이벤트 발생시키기
+                this.$emit('addEvent', this.newTodoItem);
+                this.clearInput();
+            //     var value = this.newTodoItem && this.newTodoItem.trim();
+            //     localStorage.setItem(value,value);
+            //     this.clearInput();
+            }
+        },
+        clearInput() {
+            //addTodo() 함수에 있었지만 clearInput()함수로 이동            
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 15px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
