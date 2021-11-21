25eb5d788eeb300bca0cd747708b566f7db55d41
mysoyul <vega2k_@naver.com>
2021-11-21 15:59:02 +0900

    axios vue-axios

--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 //Vue가 Vuex라는 별도의 라이브러리를 사용하겠다
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-    return arr;
-  },
-};
+//순서가 바뀌면 않됩니다.
+//Vue가 VueAxios와 axios라는 별도의 라이브러리를 사용하겠다
+Vue.use(VueAxios, axios);
+
+const api_url = 'http://localhost:4500/api/todos';
 
 //Vuex안에 포함된 Store 객체를 생성하겠다
 export const store = new Vuex.Store({
   //상태변수 선언
   state: {
-    todoItems: storage.fetch(),
+    todoItems: [],
   },
+  //서버와 통신을 담당하는 method 선언 (async)
+  actions: {},
   //상태변수를 변경하는 method 선언 (sync)
   mutations: {
+    setTodoItems(state, items) {
+      state.todoItems = items;
+    },
     addTodo(state, todoText) {
       var obj = { completed: false, item: todoText };
       //JSON.stringify는 object를 json string 으로 변환
@@ -52,8 +51,6 @@ export const store = new Vuex.Store({
       state.todoItems = [];
     },
   },
-  //서버와 통신을 담당하는 method 선언 (async)
-  actions: {},
   //상태변수를 조회하는 method 선언
   getters: {
     getTodoItems(state) {
